6072d7186138f7cfb0065615b8719e7e111a2423
Kirshin Aleksandr <aleksandr.s.kirshin@gmail.com>
2023-04-02 20:09:51 +0700

    add local state and etc

--- app/src/main/java/ru/letovo/sphere/docs/presentation/documents/list/DocumentsListScreenState.kt
+++ app/src/main/java/ru/letovo/sphere/docs/presentation/documents/list/DocumentsListScreenState.kt
@@ -1,5 +1,14 @@
 package ru.letovo.sphere.docs.presentation.documents.list
 
 data class DocumentsListScreenState(
-    val documentsListScreenName : String = "список документов"
+    val documentsListScreenName : String = "список документов",
+    val documentsList: MutableList<Document> = mutableListOf(
+        Document(name = "документ 1"),
+        Document(name = "документ 2"),
+        Document(name = "документ 3"),
+    )
+)
+
+class Document(
+    val name : String
 )
--- app/src/main/java/ru/letovo/sphere/docs/presentation/home/HomeViewModel.kt
+++ app/src/main/java/ru/letovo/sphere/docs/presentation/home/HomeViewModel.kt
@@ -1,10 +1,8 @@
 package ru.letovo.sphere.docs.presentation.home
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class HomeViewModel @Inject constructor(
@@ -14,9 +12,7 @@ class HomeViewModel @Inject constructor(
     val state = _state.asStateFlow()
 
     fun startScanning() {
-        viewModelScope.launch {
-            //
-        }
+        //
     }
 
 }
