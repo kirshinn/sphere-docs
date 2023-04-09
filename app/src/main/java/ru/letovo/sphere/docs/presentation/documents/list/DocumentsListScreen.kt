3aa35b71aeed0c859c712e9cdd95fb89ee2710ec
Kirshin Aleksandr <aleksandr.s.kirshin@gmail.com>
2023-04-09 11:10:32 +0700

    add icons in list

--- app/src/main/java/ru/letovo/sphere/docs/presentation/documents/list/DocumentsListScreen.kt
+++ app/src/main/java/ru/letovo/sphere/docs/presentation/documents/list/DocumentsListScreen.kt
@@ -1,25 +1,22 @@
 package ru.letovo.sphere.docs.presentation.documents.list
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
-import ru.letovo.sphere.docs.R
 
 @Composable
 fun DocumentsListScreen(
@@ -51,14 +48,21 @@ fun DocumentList(
 
                 Row(modifier = Modifier.padding(all = 8.dp)) {
 
-                    Image(
-                        painter = painterResource(R.drawable.card_picture),
-                        contentDescription = null,
+                    Icon(
+                        imageVector = Icons.Filled.AddCircle,
+                        contentDescription = "",
                         modifier = Modifier
                             .size(40.dp)
-                            .clip(CircleShape)
-                            .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
                     )
+
+//                    AsyncImage(
+//                        model = item.image,
+//                        contentDescription = null,
+//                        modifier = Modifier
+//                            .size(40.dp)
+//                            .clip(CircleShape)
+//                            .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+//                    )
 
                     Spacer(modifier = Modifier.width(8.dp))
 
